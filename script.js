147dd277bc3f474d5e3f8e41834d5ea081188164
Shikhar Manandhar <124882005+shikharSoori@users.noreply.github.com>
2024-02-11 15:40:39 +0545

    update

--- script.js
+++ script.js
@@ -1,43 +1,31 @@
-const slider = document.querySelector(".slider");
-const cards = document.querySelectorAll(".card");
-const prevButton = document.querySelector(".prev");
-const nextButton = document.querySelector(".next");
-let currentIndex = 0;
-const cardWidth = cards[1].offsetWidth + 20; // Including margin
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function updateSlider() {
-  const newPosition = -currentIndex * cardWidth;
-  slider.style.transform = `translateX(${newPosition}px)`;
-
-  // Remove the 'blue' class from all cards
-  cards.forEach((card) => card.classList.remove("blue"));
-
-  // Calculate the index of the last visible card
-  const lastVisibleIndex = Math.min(
-    currentIndex + Math.floor(slider.offsetWidth / cardWidth) - 1,
-    cards.length - 1
-  );
-  console.log(Math.floor(slider.offsetWidth / cardWidth));
-
-  // Add the 'blue' class to the last visible card
-  cards[lastVisibleIndex].classList.add("blue");
+function plusSlides(n) {
+  showSlides((slideIndex += n));
 }
 
-function nextSlide() {
-  currentIndex++;
-  if (currentIndex > cards.length - 1) {
-    currentIndex = 0;
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
   }
-  updateSlider();
-}
-
-function prevSlide() {
-  currentIndex--;
-  if (currentIndex < 0) {
-    currentIndex = cards.length - 1;
+  if (n < 1) {
+    slideIndex = slides.length;
   }
-  updateSlider();
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "flex";
+  slides[slideIndex - 1].classList.add("flex");
+  dots[slideIndex - 1].className += " active";
 }
-
-// Initial slider update
-updateSlider();

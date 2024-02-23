54e75ce4ddab2f05f3b63d71eb090c10ae88b887
Shikhar Manandhar <shikhar.manandhar@sooritechnology.com>
2024-02-23 20:27:04 +0545

    updated

--- productslist.js
+++ productslist.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
   var productWrapper = document.querySelector(".product-wrapper");
   var developmentPhase = document.querySelector(".development-phase");
 
-  var products = [{ name: "Product 1" }, { name: "Product 2" }];
-  // var products = [];
+  // var products = [{ name: "Product 1" }, { name: "Product 2" }];
+  var products = [];
   if (products.length > 0) {
     productWrapper.style.display = "block";
     developmentPhase.style.display = "none";
